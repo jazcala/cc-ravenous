54269875ca0ab8eff72771a7dfc66cafb5f2be6c
jazcala <jazcalabrese@gmail.com>
2024-11-11 14:24:20 -0300

    adding filters layout to searchBar section

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,6 +4,13 @@ const SearchBar = () => {
 
   return (
     <section id="search-bar">
+      <div >
+        <ul className='filters'>
+          <li>Best Match</li>
+          <li>Highest Rated</li>
+          <li>Most Reviewed</li>
+        </ul>
+      </div>
       <div>
         <input className="search-input" type='text' name='search-by-name' placeholder='Search Businesses' />
         <input className="search-input" type='text' name="search-by-location" placeholder='Where?' />
